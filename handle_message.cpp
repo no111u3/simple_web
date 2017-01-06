7846ca7d3d79b7024f86633a7f8ede3f428f5874
Boris Vinogradov <no111u3@gmail.com>
2017-01-06 20:07:10 +0300

    Performance upgrade
    [*] Change sprintf to faster digit_to_string converter

--- handle_message.cpp
+++ handle_message.cpp
@@ -4,8 +4,38 @@
 
 #include <sys/stat.h>
 #include <unistd.h>
+#include <limits>
+#include <cstring>
+#include <iostream>
 
 namespace http {
+    inline size_t uint32_to_str(uint32_t value, char *dst) {
+        static const char digits[201] =
+                "0001020304050607080910111213141516171819"
+                "2021222324252627282930313233343536373839"
+                "4041424344454647484950515253545556575859"
+                "6061626364656667686970717273747576777879"
+                "8081828384858687888990919293949596979899";
+        size_t const length = std::numeric_limits<uint32_t>::digits10;
+        size_t next = length - 1;
+        while (value >= 100) {
+            auto const i = (value % 100) * 2;
+            value /= 100;
+            dst[next] = digits[i + 1];
+            dst[next - 1] = digits[i];
+            next -= 2;
+        }
+        // Handle last 1-2 digits
+        if (value < 10) {
+            dst[next] = (char) ('0' + uint32_t(value));
+        } else {
+            auto i = uint32_t(value) * 2;
+            dst[next] = digits[i + 1];
+            dst[next - 1] = digits[i];
+        }
+        return length;
+    }
+
     ssize_t handle_message(int client, const conf::Config &config) {
         char buf[input_buffer_size] = {0};
 
@@ -52,8 +82,10 @@ namespace http {
                 } else {
                     send(client, head_error404, (size_t) size_head_error404, 0);
                 }
-                char buffer[10+4+1];
-                int size = std::sprintf(buffer, "%zu\n\n\n\n", content_len);
+                char buffer[10+4+1] = "              ";
+                int size = uint32_to_str(content_len, buffer);
+                std::memcpy(buffer + size, "\n\n\n\n", 4);
+                size += 4;
                 send(client, buffer, (size_t) size, 0);
                 if (content_len) {
                     send(client, memory_block, (size_t) content_len, 0);
